2a983bbe4c55b273d242c24f6b34f1e85a27ae91
Bogi Napoleon Wennerstrøm <bogi.wennerstrom@gmail.com>
2019-05-20 13:16:57 +0200

    modified ssh dir

--- scripts/git.sh
+++ scripts/git.sh
@@ -4,3 +4,6 @@ git config --global credential.helper "cache --timeout=1314000"
 git config --global user.email "bogi.wennerstrom@gmail.com"
 git config --global user.name "Bogi Napoleon Wennerstrøm"
 git config --global push.default current
+
+mkdir ~/.ssh
+chmod 700 ~/.ssh
