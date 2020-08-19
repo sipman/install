99a68f1ed87c480f71e3ef9c2578ffa3bd9e632b
Bogi Napoleon Wennerstrøm <bogi.wennerstrom@gmail.com>
2020-08-19 15:50:36 +0200

    corrected android install script

--- scripts/android.sh
+++ scripts/android.sh
@@ -1,18 +1,18 @@
 require java.sh
 
-# Ensure java 8 is used as android is old
-sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
-
 ANDROID_HOME="/usr/lib/android-sdk/"
-SDKMANAGER=${ANDROID_HOME}tools/bin/sdkmanager
-FILE=$(curl -s 'https://developer.android.com/studio#downloads' -o- | grep -o -m1 "sdk-tools-linux-[0-9]*.zip")
+CMDLINE_TOOLS=${ANDROID_HOME}cmdline-tools/
+TOOLS_BIN_FOLDER=${CMDLINE_TOOLS}tools/bin/
+SDKMANAGER=${TOOLS_BIN_FOLDER}sdkmanager
+FILE=$(curl -s 'https://developer.android.com/studio#downloads' -o- | grep -o -m1 "commandlinetools-linux-[0-9]*_latest.zip")
 
 echo https://dl.google.com/android/repository/$FILE
 wget https://dl.google.com/android/repository/$FILE
 
-sudo mkdir $ANDROID_HOME
-sudo mv $FILE $ANDROID_HOME
-cd $ANDROID_HOME
+sudo mkdir -p $ANDROID_HOME
+sudo mkdir -p $CMDLINE_TOOLS
+sudo mv $FILE $CMDLINE_TOOLS
+cd $CMDLINE_TOOLS
 sudo unzip $FILE
 sudo rm $FILE
 sudo chmod -R 777 $ANDROID_HOME
@@ -20,12 +20,12 @@ sudo chmod -R 777 $ANDROID_HOME
 # Fix .bashrc
 echo "# Android SDK" >> ~/.bashrc
 echo "export ANDROID_HOME=\"${ANDROID_HOME}\"" >> ~/.bashrc
-echo "export PATH=\"\${PATH}:\${ANDROID_HOME}tools/:\${ANDROID_HOME}tools/bin:\${ANDROID_HOME}platform-tools/\"" >> ~/.bashrc
+echo "export PATH=\"\${PATH}:${CMDLINE_TOOLS}tools/:${TOOLS_BIN_FOLDER}:${ANDROID_HOME}platform-tools/\"" >> ~/.bashrc
 
 # Install latest android vesion
 $SDKMANAGER --update
 yes | $SDKMANAGER --licenses
-$SDKMANAGER "platform-tools" "platforms;android-28"
+$SDKMANAGER "platform-tools" "build-tools;29.0.3" "platforms;android-29"
 yes | $SDKMANAGER --licenses
 
 unset ANDROID_HOME
--- scripts/dbeaver.sh
+++ scripts/dbeaver.sh
@@ -1,4 +1,12 @@
+wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
+echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo apt-get update && sudo apt-get install dbeaver-ce
+
+#sudo add-apt-repository ppa:serge-rider/dbeaver-ce
+#sudo apt-get update
+#sudo apt-get install dbeaver-ce
+
 # DBeaver
-wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb &&
-sudo dpkg -i dbeaver-ce_latest_amd64.deb
-rm dbeaver-ce_latest_amd64.deb
+#wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb &&
+#sudo dpkg -i dbeaver-ce_latest_amd64.deb
+#rm dbeaver-ce_latest_amd64.deb
