53523ea3ff5f052f50e3c0b4dd3ad43c19fc54c1
Bogi Napoleon Wennerstrøm <bogi.wennerstrom@gmail.com>
2020-09-02 17:33:57 +0200

    now uses sdkman instead of deprecated java links

--- scripts/java.sh
+++ scripts/java.sh
@@ -1,11 +1,8 @@
-# Java 8
+require sdkman.sh
 
-wget http://security.ubuntu.com/ubuntu/pool/universe/o/openjdk-8/openjdk-8-jdk-headless_8u191-b12-2ubuntu0.18.10.1_amd64.deb
-wget http://security.ubuntu.com/ubuntu/pool/universe/o/openjdk-8/openjdk-8-jdk_8u191-b12-2ubuntu0.18.10.1_amd64.deb
-wget http://security.ubuntu.com/ubuntu/pool/universe/o/openjdk-8/openjdk-8-jre_8u191-b12-2ubuntu0.18.10.1_amd64.deb
-wget http://security.ubuntu.com/ubuntu/pool/universe/o/openjdk-8/openjdk-8-jre-headless_8u191-b12-2ubuntu0.18.10.1_amd64.deb
+JAVA_VERSION=15
+JAVA_VERSION=$(sdk list java | grep -Po "$JAVA_VERSION.*?-open" | xargs | cut -d' ' -f6)
 
-sudo dpkg -i openjdk-8-jre-headless_8u191-b12-2ubuntu0.18.10.1_amd64.deb openjdk-8-jre_8u191-b12-2ubuntu0.18.10.1_amd64.deb openjdk-8-jdk_8u191-b12-2ubuntu0.18.10.1_amd64.deb openjdk-8-jdk-headless_8u191-b12-2ubuntu0.18.10.1_amd64.deb
-rm openjdk-8-jre-headless_8u191-b12-2ubuntu0.18.10.1_amd64.deb openjdk-8-jre_8u191-b12-2ubuntu0.18.10.1_amd64.deb openjdk-8-jdk_8u191-b12-2ubuntu0.18.10.1_amd64.deb openjdk-8-jdk-headless_8u191-b12-2ubuntu0.18.10.1_amd64.deb
-
-sudo apt install -f
+sdk install java 8.0.265-open
+sdk install java $JAVA_VERSION
+sdk use java $JAVA_VERSION
