7e7a5de5d211e6271aabc3c701c57dc0c4b1e99b
Bogi Napoleon Wennerstrøm <bogi.wennerstrom@gmail.com>
2022-04-27 18:00:09 +0200

    apt and jetbrains scripts for Ubuntu 22.04

--- scripts/jetbrains.sh
+++ scripts/jetbrains.sh
@@ -1,10 +1,17 @@
 # JetBrains Toolbox
-wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.11.4269.tar.gz
-tar -xzvf jetbrains-toolbox-1.11.4269.tar.gz
-cd jetbrains-toolbox-1.11.4269/
+
+sudo apt install fuse libfuse2
+
+URL="https://data.services.jetbrains.com/products?code=TBA&release.type=release"
+REGEX="https:\/\/download\.jetbrains\.com\/toolbox\/jetbrains-toolbox-[0-9]+\.[0-9]+\.[0-9]+\.tar\.gz"
+TOOLBOX_URL=$(curl $URL | grep -Eoh $REGEX | head -1)
+
+wget $TOOLBOX_URL
+tar -xzvf jetbrains-toolbox-*.tar.gz
+cd jetbrains-toolbox-*/
 ./jetbrains-toolbox
 cd ..
-rm -r jetbrains-toolbox-1.11.4269/
-rm jetbrains-toolbox-1.11.4269.tar.gz
+rm -r jetbrains-toolbox-*/
+rm jetbrains-toolbox-*.tar.gz
 sudo bash -c "echo 'fs.inotify.max_user_watches = 524288' >> /etc/sysctl.d/idea.conf"
 sudo sysctl -p --system
